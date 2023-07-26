2720f2e7da7ba6f22c46f857b95b987c543e6d3d
Artem Gerasimov <artemtrim@mail.ru>
2023-07-26 21:25:26 +0300

    Initial Commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Мой Macbook on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
