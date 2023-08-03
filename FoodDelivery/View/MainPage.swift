e321b92f5767249caebc99044cf91d09e0b03f6a
Artem Gerasimov <artemtrim@mail.ru>
2023-08-03 14:41:19 +0300

    add TabBar

---
+++ FoodDelivery/View/MainPage.swift
@@ -0,0 +1,83 @@
+//
+//  MainPage.swift
+//  FoodDelivery
+//
+//  Created by Мой Macbook on 03.08.2023.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    // Current tab..
+    @State var currentTab: Tab = .Home
+    
+    //Hiding TabBar..
+    init(){
+        UITabBar.appearance().isHidden = true
+    }
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            // Tab View..
+            TabView(selection: $currentTab) {
+                Home()
+                    .tag(Tab.Home)
+                Text("Liked")
+                    .tag(Tab.Liked)
+                Text("Profile")
+                    .tag(Tab.Profile)
+                Text("Cart")
+                    .tag(Tab.Cart)
+            }
+            //Custom TabBar..
+            HStack(spacing: 0) {
+                ForEach(Tab.allCases, id: \.self) {tab in
+                    
+                    Button {
+                        // updating tab..
+                        currentTab = tab
+                    } label: {
+                        Image(tab.rawValue)
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 22, height: 22)
+                        // Applying little shadow at bg..
+                            .background(
+                            
+                            Color("myColor2")
+                                .opacity(0.1)
+                                .cornerRadius(5)
+                            // Blurring..
+                                .blur(radius: 5)
+                            // Making little big ..
+                                .padding(-7)
+                                .opacity(currentTab == tab ? 1 : 0)
+                            )
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(currentTab == tab ? Color("myColor2") : Color.black.opacity(0.3))
+                    }
+                }
+            }
+            .padding([.horizontal, .top])
+            .padding(.bottom, 10)
+        }
+    }
+}
+
+struct MainPage_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPage()
+    }
+}
+
+//Making Case Iteratable..
+//Tab Case..
+enum Tab: String, CaseIterable {
+    
+    //Raw value must be image name in asset..
+    case Home = "house"
+    case Liked = "heart"
+    case Profile = "person"
+    case Cart = "cart"
+}
---
+++ FoodDelivery/View/TabViews/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  FoodDelivery
+//
+//  Created by Мой Macbook on 03.08.2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
