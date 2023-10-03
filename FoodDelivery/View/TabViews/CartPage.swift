a9c723ebaf87353af05f9f181d45dd3ea0eb9af6
Artem Gerasimov <artemtrim@mail.ru>
2023-10-03 13:14:49 +0300

    add CartPage

---
+++ FoodDelivery/View/TabViews/CartPage.swift
@@ -0,0 +1,20 @@
+//
+//  CartPage.swift
+//  FoodDelivery
+//
+//  Created by Мой Macbook on 27.09.2023.
+//
+
+import SwiftUI
+
+struct CartPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CartPage_Previews: PreviewProvider {
+    static var previews: some View {
+        CartPage()
+    }
+}
